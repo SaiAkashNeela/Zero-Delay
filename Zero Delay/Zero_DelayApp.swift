13f5ea4b35e95d654f7286d438fc35bebd25d34f
saiakashneela <saiakashneela@outlook.com>
2025-07-14 20:20:16 +0100

    Initial Commit

---
+++ Zero Delay/Zero_DelayApp.swift
@@ -0,0 +1,17 @@
+//
+//  Zero_DelayApp.swift
+//  Zero Delay
+//
+//  Created by Sai Akash Neela on 14/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct Zero_DelayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Zero DelayTests/Zero_DelayTests.swift
@@ -0,0 +1,17 @@
+//
+//  Zero_DelayTests.swift
+//  Zero DelayTests
+//
+//  Created by Sai Akash Neela on 14/07/2025.
+//
+
+import Testing
+@testable import Zero_Delay
+
+struct Zero_DelayTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
